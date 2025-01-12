055cf297348bfd9dc1ac407f583da872e18a3e47
Crhistian Ninalaya <133820344+CrhistianNinalaya@users.noreply.github.com>
2025-01-11 20:16:35 -0500

    Add meal management functionality with database integration; implement getMeals function and MealItem component styling

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,10 @@
 import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/MealsGrid';
-const MealsPage = () => {
+import { getMeals } from '@/lib/meals';
+
+const MealsPage = async () => {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +20,7 @@ const MealsPage = () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
--- components/meals/MealItem.jsx
+++ components/meals/MealItem.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import classes from './MealItem.module.css';
 
 const MealItem = ({ title, slug, image, summary, creator }) => {
   return (
