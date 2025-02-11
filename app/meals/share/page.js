43dfeeb0f6a800e99c9a4c7afc50b512d3e30df2
Crhistian Ninalaya <133820344+CrhistianNinalaya@users.noreply.github.com>
2025-02-10 21:17:37 -0500

    Refactor ShareMealPage to use MealsFormSubmit component for form submission; implement loading state

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,7 @@
 import ImagePicker from '@/components/meals/ImagePicker/ImagePicker';
 import classes from './page.module.css';
 import { shareMeal } from './share.server.actions';
+import { MealsFormSubmit } from '@/components/meals/MealsFormSubmit';
 
 const ShareMealPage = () => {
   return (
@@ -42,7 +43,7 @@ const ShareMealPage = () => {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
