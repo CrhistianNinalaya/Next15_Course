263fef8d34138c789ab5c3f5daee90e49f2bfbd5
Crhistian Ninalaya <133820344+CrhistianNinalaya@users.noreply.github.com>
2025-03-13 22:34:04 -0500

    Refactor news page to use NewsList component for better structure and readability

--- next-news/src/app/news/page.tsx
+++ next-news/src/app/news/page.tsx
@@ -1,24 +1,12 @@
+import NewsList from '@/components/NewsList/NewsList';
 import { DUMMY_NEWS } from '@/dummy-news';
-import Image from 'next/image';
-import Link from 'next/link';
 
 const News: React.FC = () => {
   return (
-    <ul className="news-list">
-      {DUMMY_NEWS.map((news) => (
-        <li key={news.id}>
-          <Link href={`/news/${news.id}`}>
-            <Image
-              src={`/assets/images/news/${news.image}`}
-              alt={news.title}
-              width={300}
-              height={200}
-            />
-            <span>{news.title}</span>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h1>News Page </h1>
+      <NewsList news={DUMMY_NEWS} />;
+    </>
   );
 };
 
